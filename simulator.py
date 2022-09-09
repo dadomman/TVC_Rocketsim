127b2f097e009b6883f8381caa2cbbf1f47671d5
dadomman <dkeeler@cps.edu>
2022-09-09 21:03:14 +0000

    mandarin work

--- simulator.py
+++ simulator.py
@@ -1,1 +1,17 @@
 # Python Based TVC simulator
+#Imports 
+import turtle
+class constants:
+    gravity = 9.81
+    burnTime = 
+
+#RocketSpecs & Inputs
+totalMass = float(input("Rocket Mass, g"))
+COMdistfromnoseconetop = float(input("Center of Mass distance from tip of nose cone, cm"))
+COMdistfrommotor = float(input("Center of Mass distance from motor/bottom, cm"))
+distCM = COMdistfrommotor + COMdistfromnoseconetop
+# If possible implement live wind update, see if that can reinforce gyro readings
+windSpeed = float(input("Wind Speed, mph"))
+windDir = str(input("Cardinal Direction relative to rocket (n, s, e, w, nw, ne, sw, se, nne, nnw, sse, ssw, see, sww, nee, nww"))
+def windDirAssign(windDir):
+    if 
